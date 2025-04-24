9c6ed14b4d0e8313298f8fad92d96b173b1d17e9
Mike Landau <mikeland86@gmail.com>
2025-04-24 09:01:47 -0700

    [go-monorepo] add DEVBOX_GO_MONOREPO_EXCLUDE

--- go-monorepo/for_each_gomod.sh
+++ go-monorepo/for_each_gomod.sh
@@ -3,6 +3,11 @@
 # every module.
 pwd="$(pwd)"
 for dir in $(go list -m -f '{{`{{.Dir}}`}}'); do
+  # Skip directories that match the exclude pattern if set
+  if [ -n "${DEVBOX_GO_MONOREPO_EXCLUDE}" ] && [[ "$dir" =~ ${DEVBOX_GO_MONOREPO_EXCLUDE} ]]; then
+    echo "Skipping excluded directory: $dir"
+    continue
+  fi
   echo "$dir: $*" && cd "$dir" && "$@" || exit $?
 done
 cd "$pwd" || return
